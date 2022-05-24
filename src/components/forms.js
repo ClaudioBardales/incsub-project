9ebf4c6669047fb1ae0605493e935c26e92f5a01
Claudio <claudioeduardobardales@gmail.com>
2022-05-23 22:47:36 -0700

    Adding styles to form

--- src/components/forms.js
+++ src/components/forms.js
@@ -4,14 +4,14 @@ import styled from 'styled-components';
 const forms = () => {
   return (
     <Container>
-        <div>
+        <Wrapper>
             <h1>Let's set up your account</h1>
             <p>Already have a account? <span>Sign in</span></p>
             <form>
                 <input type="text" name='name' />
                 <input type="email" name='email'/>
-                <select name="" id="" placeholder='I would describe my user type as'>
-                    <option value="">Developer</option>
+                <select>
+                    <option value="">I would describe my user type as</option>
                     <option value="">Developer</option>
                 </select> 
                 <input type="password" />
@@ -19,7 +19,7 @@ const forms = () => {
                 <button type="submit">Next</button>
                 <p>By clicking the "Next" button, you agree to creating a free, and to <span>Terms of Service</span> and <span>Privacy Policy</span></p>
             </form>
-        </div>
+        </Wrapper>
     </Container>
   )
 }
@@ -27,7 +27,51 @@ const forms = () => {
 const Container = styled.div`
 min-height: 50vh;
 width: 100%;
+display: flex;
+justify-content: center;
+align-items: center ;
 background-color: white;
+`
+
+const Wrapper = styled.div`
+display: flex;
+gap: 1.5rem;
+flex-direction: column;
+margin: auto;
+h1,p{
+  width: 60%;
+  margin: auto;
+}
+form{
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  width: 60%;
+  margin: auto;
+  p{
+    width: 100%;
+    margin: auto;
+    line-height: 2;
+  }
+  input,select{
+    padding: 0.5rem;
+  }
+  button{
+    padding: 1rem;
+    border-radius: 5px;
+    border: none;
+    color: white;
+    font-weight: bold;
+    letter-spacing: 1px;
+    background-color: #276efa;
+    cursor: pointer;
+  }
+}
+span{
+  color: #276efa;
+  font-weight: bold;
+}
+
 `
 
 export default forms
